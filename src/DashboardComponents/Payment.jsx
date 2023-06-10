bdcb9b500d6d42037b3f002948b1eada420f710f
jahid902 <jahidjr902@gmail.com>
2023-06-10 13:25:32 +0600

    completed til payment intent need to implement post method now

--- src/DashboardComponents/Payment.jsx
+++ src/DashboardComponents/Payment.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Title from '../Shared/Title';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import CheckoutForm from '../PaymentForm/CheckoutForm';
+import { useState } from 'react';
+
+const stripePromise = loadStripe(`${import.meta.env.VITE_PK_KEY}`);
 
 const Payment = () => {
 
     const {id} = useParams()
-    console.log(id);
+    const [classInfo, setClassInfo] = useState(null);
+    
+    useEffect(()=>{
+        fetch(`${import.meta.env.VITE_api_url}/oneClass/${id}`)
+        .then(res=> res.json())
+        .then(data=> {
+            setClassInfo(data)
+        })
+    },[])
+    
 
     return (
         <>
@@ -15,6 +30,12 @@ const Payment = () => {
         subText=""
         colorText=""
         ></Title>
+        
+        <Elements stripe={stripePromise}>
+            <div className='w-3/4 mx-auto bg-gray-200 rounded-md p-10' >
+            <CheckoutForm classInfo={classInfo}></CheckoutForm>
+            </div>
+        </Elements>
         
         </>
     );
---
+++ src/PaymentForm/CheckoutForm.jsx
@@ -0,0 +1,123 @@
+import {CardElement, useElements, useStripe} from '@stripe/react-stripe-js';
+import './CheckoutForm.css'
+import { useContext, useState } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { useEffect } from 'react';
+
+
+
+const CheckoutForm = ({classInfo}) => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const {user} = useContext(AuthContext)
+    const [showError, setShowError] = useState('')
+    const [clientSecret, setClientSecret] = useState('');
+
+
+    useEffect(()=>{
+        if(classInfo?.price){
+            fetch(`${import.meta.env.VITE_api_url}/create-payment-intent`,{
+                method: 'POST',
+                headers:{
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({price: classInfo.price})
+            })
+            .then(res=>res.json())
+            .then(data=> {
+                setClientSecret(data.clientSecret)
+            })
+        }
+    },[classInfo])
+  
+    const handleSubmit = async (event) => {
+      // Block native form submission.
+      event.preventDefault();
+  
+      if (!stripe || !elements) {
+        // Stripe.js has not loaded yet. Make sure to disable
+        // form submission until Stripe.js has loaded.
+        return;
+      }
+  
+      // Get a reference to a mounted CardElement. Elements knows how
+      // to find your CardElement because there can only ever be one of
+      // each type of element.
+      const card = elements.getElement(CardElement);
+  
+      if (card == null) {
+        return;
+      }
+  
+      // Use your card Element with other Stripe.js APIs
+      const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card,
+      });
+  
+      if (error) {
+        console.log('[error]', error);
+        setShowError(error.message)
+      } else {
+        console.log('[PaymentMethod]', paymentMethod);
+      }
+
+      const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+        clientSecret,
+        {
+          payment_method: {
+            card: card,
+            billing_details: {
+              name: user?.displayName,
+              email: user?.email
+            },
+          },
+        },
+      );
+
+      if (confirmError) {
+        console.log('[error]', confirmError);
+        setShowError(confirmError.message)
+      } else {
+        console.log('[paymentIntent]', paymentIntent);
+        if(paymentIntent.status === 'succeeded'){
+            
+
+        }
+
+
+      }
+
+    };
+  
+    return (
+      <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
+              },
+            },
+          }}
+        />
+        <button className='btn btn-outline btn-secondary btn-sm' type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+      {showError && <p className='text-red-500 mt-4 ml-4'>{showError}</p>} 
+      </>
+    );
+  };
+
+
+export default CheckoutForm;
